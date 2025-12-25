bd998f5172ebd513431be55b5e10f87748fa4107
PP Toolbox <82496116+pptoolbox@users.noreply.github.com>
2025-12-25 17:05:35 +0530

    Kubuntu Initializer

--- Kubuntu_init.sh
+++ Kubuntu_init.sh
@@ -1,6 +1,76 @@
 #!/bin/bash
 
-# Function to ask for confirmation
+# Upgrade System (Mandatory)
+sudo apt update
+
+# Update system packages (Mandatory)
+sudo apt full-upgrade -y
+
+# Install/configure utilities (Mandatory)
+sudo apt install curl wget partitionmanager filelight exfat-fuse exfatprogs vlc kdeconnect -y
+
+# Prompt user for automated or manual setup
+echo "Do you want to do automated setup? (Type 'Yup' to proceed or [ENTER] to skip and continue manually)"
+read ans
+
+ans=$(echo "$ans" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$ans" == "yup" ]]; then
+    sudo apt install papirus-icon-theme papirus-colors -y
+    sudo apt install  bibata-cursor-theme -y
+
+    sudo apt install starship -y
+    echo eval "$(starship init bash)" >> ~/.bashrc
+
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
+    rm google-chrome-stable_current_amd64.deb
+
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+    sudo apt update
+    sudo apt install brave-browser -y
+
+    wget https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors_amd64.deb
+    sudo apt install ./onlyoffice-desktopeditors_amd64.deb -y
+    rm onlyoffice-desktopeditors_amd64.deb
+    sudo apt purge libreoffice libreoffice-* -y
+
+    sudo apt install build-essential make cmake gcc g++ -y
+    wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode-stable_current_amd64.deb
+    sudo apt install ./vscode-stable_current_amd64.deb -y
+    rm vscode-stable_current_amd64.deb
+
+    sudo apt install inkscape -y
+
+    sudo apt install yakuake -y
+
+    if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+        # Install Espanso for Wayland
+        wget https://github.com/espanso/espanso/releases/latest/download/espanso-debian-wayland-amd64.deb
+        sudo apt install ./espanso-debian-wayland-amd64.deb -y
+        rm espanso-debian-wayland-amd64.deb
+    else
+        # Install Espanso for X11
+        wget https://github.com/espanso/espanso/releases/latest/download/espanso-debian-x11-amd64.deb
+        sudo apt install ./espanso-debian-x11-amd64.deb -y
+        rm espanso-debian-x11-amd64.deb
+    fi
+    espanso service register
+    espanso start
+
+    sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/pptoolbox/esc/main/install.sh)"
+
+    sudo mv wallpapers /usr/local/share/
+
+    # Mandatory cleanup
+    sudo apt purge snap snapd plasma-discover-backend-snap htop -y
+    sudo apt autopurge -y
+    sudo apt clean
+    sudo apt autoclean
+
+    echo "Kubuntu initialization completed. Enjoy!"
+elif [[ -z "$ans" ]]; then
 confirm() {
     while true; do
         read -p "$1 [Y/n] " response
@@ -14,15 +84,6 @@ confirm() {
         fi
     done
 }
-
-# Upgrade System (Mandatory)
-sudo apt update
-
-# Update system packages (Mandatory)
-sudo apt full-upgrade -y
-
-# Install/configure utilities (Mandatory)
-sudo apt install curl wget partitionmanager filelight exfat-fuse exfatprogs vlc kdeconnect -y
 
 # Install themes and cursors
 if confirm "Do you want to install papirus icon theme?"; then
@@ -85,12 +146,12 @@ if confirm "Do you want to install Espanso (text expander)?"; then
     if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
         # Install Espanso for Wayland
         wget https://github.com/espanso/espanso/releases/latest/download/espanso-debian-wayland-amd64.deb
-        sudo apt install espanso-debian-wayland-amd64.deb -y
+        sudo apt install ./espanso-debian-wayland-amd64.deb -y
         rm espanso-debian-wayland-amd64.deb
     else
         # Install Espanso for X11
         wget https://github.com/espanso/espanso/releases/latest/download/espanso-debian-x11-amd64.deb
-        sudo apt install espanso-debian-x11-amd64.deb -y
+        sudo apt install ./espanso-debian-x11-amd64.deb -y
         rm espanso-debian-x11-amd64.deb
     fi
     espanso service register
@@ -138,3 +199,7 @@ sudo apt clean
 sudo apt autoclean
 
 echo "Kubuntu initialization completed. Enjoy!"
+
+else
+    echo "Invalid input. Please run the script again and provide a valid response."
+fi
