4d9b1fd7e62f8a74a3f8a370a03355a245b563d3
PP Toolbox <82496116+pptoolbox@users.noreply.github.com>
2026-02-03 15:00:40 +0530

    Kali Linux KDE Initialization

--- Kali_Linux-KDE_init.sh
+++ Kali_Linux-KDE_init.sh
@@ -9,7 +9,7 @@ sudo apt update
 sudo apt full-upgrade -y
 
 # Install/configure utilities (Mandatory)
-sudo apt install curl wget kio-admin partitionmanager filelight exfatprogs vlc kdeconnect okular bleachbit -y
+sudo apt install curl wget kio-admin partitionmanager filelight exfatprogs vlc kdeconnect plasma-widgets-addons okular bleachbit -y
 
 # Enable required services (Mandatory)
 sudo systemctl enable --now bluetooth.service
